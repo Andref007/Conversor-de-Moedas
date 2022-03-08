1819806b057787e81776b5f6c7f7b71a58a1cd32
André Felipe <andrefps007@hotmail.com>
2022-03-08 17:40:13 -0300

    primeiro commit conversor de moedas

---
+++ script.js
@@ -0,0 +1,56 @@
+function ConverterDolar() {
+  var valorElemento = document.getElementById("valor1")
+  var valor = valorElemento.value
+  var valorEmDolarNumerico = parseFloat(valor)
+
+
+  var valorEmReal = valorEmDolarNumerico * 5.09
+  
+
+  var elementoValorConvertido = document.getElementById("valorConvertido1")
+var valorConvertido = "O resultado em real é R$ " + valorEmReal
+elementoValorConvertido.innerHTML = valorConvertido
+} 
+
+function ConverterEuro() {
+  var valorElemento = document.getElementById("valor2")
+  var valor = valorElemento.value
+  var valorEmEuroNumerico = parseFloat(valor)
+
+
+  var valorEmReal = valorEmEuroNumerico * 5.55
+  
+
+  var elementoValorConvertido = document.getElementById("valorConvertido2")
+var valorConvertido = "O resultado em real é R$ " + valorEmReal
+elementoValorConvertido.innerHTML = valorConvertido
+} 
+
+function ConverterPesoArgentino() {
+  var valorElemento = document.getElementById("valor3")
+  var valor = valorElemento.value
+  var valorEmPesoArgentinoNumerico = parseFloat(valor)
+
+
+  var valorEmReal = valorEmPesoArgentinoNumerico * 0.047
+  
+
+  var elementoValorConvertido = document.getElementById("valorConvertido3")
+var valorConvertido = "O resultado em real é R$ " + valorEmReal
+elementoValorConvertido.innerHTML = valorConvertido
+} 
+
+function ConverterIene() {
+  var valorElemento = document.getElementById("valor4")
+  var valor = valorElemento.value
+  var valorEmIeneNumerico = parseFloat(valor)
+
+
+  var valorEmReal = valorEmIeneNumerico * 0.044
+  
+
+  var elementoValorConvertido = document.getElementById("valorConvertido4")
+var valorConvertido = "O resultado em real é R$ " + valorEmReal
+elementoValorConvertido.innerHTML = valorConvertido
+} 
+
